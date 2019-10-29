71adc09c2475f3156130e1b1a14f61d0f935cbf9
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-10-29 11:02:16 +0600

    backend admin's mastering complete

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()  && auth()->user()->role->id == 1) {
-            return redirect('admin.dashboard');
+            return redirect('admin/dashboard');
         }elseif (Auth::guard($guard)->check()  && auth()->user()->role->id == 2){
-            return redirect('author.dashboard');
+            return redirect('author/dashboard');
         }else{
             return $next($request);
         }
