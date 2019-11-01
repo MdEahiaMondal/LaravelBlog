b1302dbbb18c23b6735ec3b6e99f2439293a5a90
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-01 09:46:36 +0600

    post view count done

--- app/Http/Controllers/Frontend/PostController.php
+++ app/Http/Controllers/Frontend/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Post;
+use Session;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,6 +14,13 @@ class PostController extends Controller
 
          $post = Post::where('slug', $slug)->first();
         $randomPosts = Post::all()->random(3);
+
+        // post view count
+        $blogKey = "blog_".$post->id;
+        if (!Session::has($blogKey)){
+            $post->increment('view_count');
+            Session::put($blogKey, 1);
+        }
 
         return view('frontend.single-post.index', compact('post', 'randomPosts'));
     }
