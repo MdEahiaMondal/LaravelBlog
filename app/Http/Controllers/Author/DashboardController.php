8a3bb8fe5ede3a9c465e15f59c89dffefd341bfc
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-02 07:01:11 +0600

    author can see popular post, total view count, comments, publish and pending post  done

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -9,6 +9,20 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('author.dashboard');
+        $user = auth()->user();
+        $posts = $user->posts;
+        $popular_posts = $user->posts()
+          ->withCount('comments')
+          ->withCount('favorite_to_users')
+          ->orderBy('view_count', 'desc')
+          ->orderBy('favorite_to_users_count', 'desc')
+          ->orderBy('comments_count', 'desc')
+          ->take(5)
+          ->get();
+        $total_pending = $posts->where('is_approved', false)->count();
+        $total_post_view = $posts->sum('view_count');
+
+
+        return view('author.dashboard', compact('user','posts','popular_posts','total_pending', 'total_post_view'));
     }
 }
